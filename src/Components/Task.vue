0fa4ae0a8c0fc929eadcaa6baed3ee5272cfa6e3
Jerickson Tomajin <tomajinj@gmail.com>
2023-06-11 19:30:33 +0800

    init

--- src/Components/Task.vue
+++ src/Components/Task.vue
@@ -1,5 +1,5 @@
 <template>
-    <!-- ALL TASK -->
+    <!-- ALL TASKS -->
     <div :class="[allTask ? 'show' : 'hide', 'task']">
         <div :class="[task.complete ? 'complete' : '', 'task']">
             <Button 
@@ -9,9 +9,37 @@
                 @btn-click="$emit('toggle-complete', task.id)"
             />
             <h3>{{ task.text }}</h3>
+
+            <Button 
+                class="delete-task-btn" 
+                text="Delete"
+                bg_color="rgb(255, 65, 65)"
+                @btn-click="$emit('delete-task', task.id)"
+            />
         </div>
     </div>
 
+    <!-- ACTIVE TASKS -->
+    <div :class="[showActive ? 'show' : 'hide', 'task']">
+        <div :class="[!task.complete ? '' : 'hide', 'task']">
+            <Button 
+                class="btn" 
+                @btn-click="$emit('toggle-complete', task.id)"
+                text="" 
+                :bg_color="[task.complete ? 'black' : 'transparent']" 
+            />
+            <h3>{{ task.text }}</h3>
+
+            <Button 
+                class="delete-task-btn" 
+                text="Delete"
+                bg_color="rgb(255, 65, 65)"
+                @btn-click="$emit('delete-task', task.id)"
+            />
+        </div>
+    </div>
+
+    <!-- COMPLETE TASKS -->
     <div :class="[showCompleted ? 'show' : 'hide', 'task']">
         <div :class="[task.complete ? 'complete' : 'hide', 'task']">
             <Button 
@@ -21,6 +49,13 @@
                 :bg_color="[task.complete ? 'black' : 'transparent']" 
             />
             <h3>{{ task.text }}</h3>
+
+            <Button 
+                class="delete-task-btn" 
+                text="Delete"
+                bg_color="rgb(255, 65, 65)"
+                @btn-click="$emit('delete-task', task.id)"
+            />
         </div>
     </div>
 </template>
@@ -33,12 +68,13 @@
         props: {
             task: Object,
             allTask: Boolean,
+            showActive: Boolean,
             showCompleted: Boolean,
         },
         components: {
             Button,
         },
-        emits: ['toggle-complete']
+        emits: ['toggle-complete', 'delete-task']
     }
 </script>
 
@@ -48,17 +84,27 @@
         align-items: center;
         justify-content: start;
         gap: 24px;
+        margin-bottom: 8px;
     }
 
     .complete {
         color: rgb(112, 112, 112);
-        text-decoration: line-through;
+        h3 {
+            text-decoration: line-through;
+        }
     }
 
     .btn {
         padding: 12px;
         border: 1px solid #444;
         border-radius: 50px;
+    }
+
+    .delete-task-btn {
+        margin-left: auto;
+        border: none;
+        border-radius: 8px;
+        color: white;
     }
 
     .show {
