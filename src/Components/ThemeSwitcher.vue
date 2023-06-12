69e4398e4182bd5884973e6a867e0559886f6ee5
Jerickson Tomajin <tomajinj@gmail.com>
2023-06-13 00:02:19 +0800

    init

---
+++ src/Components/ThemeSwitcher.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <Button @btn-click="toggleTheme" />
+    </div>
+</template>
+
+<script>
+    import Button from './Button.vue';
+
+    export default {
+        name: 'ThemeSwitcher',
+        components: {
+            Button,
+        },
+        methods: {
+            toggleTheme() {
+                this.$emit('toggle-theme')
+            }
+        },
+    }
+</script>
+
+<style lang="scss">
+    .dark {
+        --clr-red: red;
+    }
+
+    p {
+        color: var(--clr-red);
+    }
+</style>
