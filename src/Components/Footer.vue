8ad4f31fc0de9cc0323dbcb1bd18735cdf9152e2
Jerickson Tomajin <tomajinj@gmail.com>
2023-06-13 18:32:49 +0800

    init

--- src/Components/Footer.vue
+++ src/Components/Footer.vue
@@ -1,40 +1,42 @@
 <template>
-    <div class="items-left">
-        <p>{{ items }} item/s left</p>
-    </div>
-    
-    <div class="buttons">
-        <!-- Show All -->
-        <Button
-            @click="$emit('show-all')"
-            text="All"
-            bg_color="transparent"
-            :class="[allTask ? 'active' : '']" 
-        />
-
-        <!-- Show Active -->
-        <Button
-            @click="$emit('show-active')"
-            text="Active"
-            bg_color="transparent"
-            :class="[showActive ? 'active' : '']"
-        /> 
+    <div class="footer">
+        <div class="items-left">
+            <p>{{ items }} item/s left</p>
+        </div>
         
-        <!-- Show Completed -->
+        <div class="buttons">
+            <!-- Show All -->
+            <Button
+                @click="$emit('show-all')"
+                text="All"
+                bg_color="transparent"
+                :class="[allTask ? 'active' : '']" 
+            />
+    
+            <!-- Show Active -->
+            <Button
+                @click="$emit('show-active')"
+                text="Active"
+                bg_color="transparent"
+                :class="[showActive ? 'active' : '']"
+            /> 
+            
+            <!-- Show Completed -->
+            <Button 
+                @click="$emit('show-completed')"
+                text="Completed" 
+                bg_color="transparent" 
+                :class="[showCompleted ? 'active' : '']"    
+            /> 
+        </div>
+    
+        <!-- Clear completed -->
         <Button 
-            @click="$emit('show-completed')"
-            text="Completed" 
+            @click="$emit('clear-completed')"
+            text="Clear completed" 
             bg_color="transparent" 
-            :class="[showCompleted ? 'active' : '']"    
-        /> 
+            class="btn" />
     </div>
-
-    <!-- Clear completed -->
-    <Button 
-        @click="$emit('clear-completed')"
-        text="Clear completed" 
-        bg_color="transparent" 
-        class="btn" />
 </template>
 
 <script>
@@ -56,19 +58,60 @@
 </script>
 
 <style lang="scss" scoped>
+    .footer {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: {
+            inline: 1rem;
+            bottom: .5rem;
+        };
+        
+        position: relative;
+    }
+
     .btn {
-        color: white;
+        color: var(--clr-text);
         padding: 0;
+        transition: color 150ms ease-in;
+
+        &:hover {
+            color: var(--clr-hover);
+        }
     }
 
     .buttons {
+        background-color: var(--clr-background-todo);
+        padding: 1rem;
+        width: 100%;
+
         display: flex;
         align-items: center;
+        justify-content: center;
         gap: 8px;
+
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        bottom: -5rem;
+
+        transition: all 500ms ease-in;
+
+        @media only screen and (min-width: 600px) {
+            padding: unset;
+            width: auto;
+
+            position: unset;
+            transform: unset;
+        }
     }
 
+    .items-left {
+        color: var(--clr-text);
+    }
+    
     .active {
-        color: rgb(0, 162, 255);
+        color: var(--clr-accent);
         font-weight: bold;
     }
 </style>
